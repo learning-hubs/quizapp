facc2d4de689193537389d256169e9e6d9fd884a
Suman Kumari <sumankumariyadav2710@gmail.com>
2026-03-03 11:34:16 +0530

    added submitQuiz API in monolith application of Quiz APP

--- src/main/java/com/telusko/quizapp/controller/QuizController.java
+++ src/main/java/com/telusko/quizapp/controller/QuizController.java
@@ -1,6 +1,7 @@
 package com.telusko.quizapp.controller;
 
 import com.telusko.quizapp.model.QuestionWrapper;
+import com.telusko.quizapp.model.Response;
 import com.telusko.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -24,8 +25,8 @@ public class QuizController {
         return quizService.geQuizQuestions(id);
     }
 
-//    @PostMapping("submit/{id}")
-//    public ResponseEntity<Integer> submitQuiz(@PathVariable String id, @RequestBody) {
-//
-//    }
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses) {
+        return quizService.calculateResult(id, responses);
+    }
 }
